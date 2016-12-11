06a3543f995d6883c8589e524f3ca265d4b85828
monikastan <monikastan@o2.pl>
2016-12-11 15:27:14 +0100

    added Product.php

--- src/Product.php
+++ src/Product.php
@@ -1,8 +1,50 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Class Product:
  */
 
+class Product {
+    private $id;
+    private $name;
+    private $price;
+    private $description;
+    private $quantity;
+    
+    public function __construct() {
+        $this->id = -1;
+        $this->name = "";
+        $this->price = 0;
+        $this->description = "";
+        $this->quantity = 0;
+    }
+    /*
+     * Sets and Gets mathods:
+     */
+    public function setName($name) {
+        $this->name = $name;
+    }
+    public function setPrice($name) {
+        $this->name = $name;
+    }
+    public function setDescription($description) {
+        $this->description = $description;
+    }
+    public function setQuantity($quantity) {
+        $this->quantity = $quantity;
+    }
+    public function getId() {
+        return $this->id;
+    }
+    public function getName() {
+        return $this->name;
+    }
+    public function getPrice() {
+        return $this->price;
+    }
+    public function getDescription() {
+        return $this->description;
+    }
+    public function getQuantity() {
+        return $this->quantity;
+    }
+}
